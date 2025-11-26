32061c479811141ee0d99abac75f17b6894bcbc0
Amjad Pauzi <amjadpauzi@gmail.com>
2025-11-26 21:05:57 +0800

    refs : spring app

---
+++ PaymentApp/src/main/java/com/Portfolio/PaymentApp/Payment.java
@@ -0,0 +1,13 @@
+package com.Portfolio.PaymentApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Payment {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentAppApplication.class, args);
+	}
+
+}
